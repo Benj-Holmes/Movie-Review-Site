dc3d07df33f225f58a7bd5d261d5e530a0fad2a8
Benjamin Holmes <HolmesBen@Proton.me>
2025-01-19 23:24:25 +0000

    Added Bootstrap, Linked up first Components

---
+++ frontend/src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+    return (
+        <div className='navbar w-100 bg-black text-white g-0 py-3'>
+            <h1>Movie Forum</h1>
+        </div>
+    );
+}
+
+export default Header;
