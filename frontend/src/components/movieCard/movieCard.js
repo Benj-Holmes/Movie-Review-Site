6685ec211e0f90a4bb61d4d0b06831bf1fb01fb5
Benjamin Holmes <HolmesBen@Proton.me>
2025-02-03 22:24:46 +0000

    Update movieCard.js

--- frontend/src/components/movieCard/movieCard.js
+++ frontend/src/components/movieCard/movieCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MovieCard = (props) => {
     return (
         <div>
-            <h5>{props.title}</h5>
+            <h5>{props.title} 555</h5>
             <img src={props.poster} />
             <p>{props.release.substring(0,4)} -------- {props.vote} /10</p>
         </div>
